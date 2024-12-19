c692ffe13b1ad324b2ebd5be3cd90fec934d1275
ecomback-official <ecomback60@gmail.com>
2024-12-19 21:03:41 +0500

    add warnings and labels to mobile and desktop views

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -16,28 +16,28 @@ export const Socialicons = (params) => {
       <ul>
         {socialprofils.twitter && (
           <li>
-            <a aria-label="follow me on Twitter" href={socialprofils.twitter}>
+            <a aria-label="follow me on Twitter, this link will open in a new tab" target="_blank" href={socialprofils.twitter}>
               <FaTwitter />
             </a>
           </li>
         )}
         {socialprofils.github && (
           <li>
-            <a aria-label="Follow me on Github" href={socialprofils.github}>
+            <a aria-label="Follow me on Github, this link will open in a new tab" target="_blank" href={socialprofils.github}>
               <FaGithub />
             </a>
           </li>
         )}
         {socialprofils.facebook && (
           <li>
-            <a aria-label="Follow me on facebook" href={socialprofils.facebook}>
+            <a aria-label="Follow me on facebook, this link will open in a new tab" target="_blank" href={socialprofils.facebook}>
               <FaFacebookF />
             </a>
           </li>
         )}
         {socialprofils.linkedin && (
           <li>
-            <a aria-label="Follow me on Linkedin" href={socialprofils.linkedin}>
+            <a aria-label="Follow me on Linkedin, this link will open in a new tab" target="_blank" href={socialprofils.linkedin}>
               <FaLinkedin />
             </a>
           </li>
