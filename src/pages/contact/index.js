6793b1d3d37d9bb415765089d777dd598802f5f5
Hamza Zahid <hamzaruel361@gmail.com>
2025-02-08 14:29:21 +0500

    index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -111,7 +111,7 @@ export const ContactUs = () => {
             </address>
             <p>{contactConfig.description}</p>
             <div className="cv-btn">
-<a href="https://drive.google.com/file/d/1BAAFVhbZswZUQJ2oSvTgPUhlR38ltYuZ/view?usp=drive_link" target="_blank">
+<a href="https://drive.google.com/file/d/1jqq5OLDDTJ6lNrhVcIU65LkoQDS13WK9/view?usp=drive_link" target="_blank">
             Download CV
           </a>
   </div>
